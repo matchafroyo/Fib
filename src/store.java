9486dc396baf97920700dfb0711547dd50567d1c
matchafroyo <yiying@yiying.org>
2014-07-15 23:33:23 -0400

    checkpoint

--- src/store.java
+++ src/store.java
@@ -1,10 +1,10 @@
 import java.io.*;
 
-public class Store {
+public class store {
 	
-	private final FibCalc fibCalc;
+	private static fib_calc fibCalc;
 	static int MAX_ARRAY_SIZE = 100;
-	private int[] fibStore = new int[MAX_ARRAY_SIZE];
+	private static int[] fibStore = new int[MAX_ARRAY_SIZE];
 
 	public static void main(String[] args) {
 		// let's first test that the arguments we're taking are integral inputs
@@ -16,22 +16,24 @@ public class Store {
 		}	
 	}
 	
-	private int[] parseInt(String[] args) {
+	private static int[] parseInt(String[] args) {
 		if (args.length <= 0) {
 			System.err.println("Not enough arguments. Please retry.");
 			System.exit(-1);
 		}
 		
 		final int[] argsToInt = new int[args.length];
-	
+		int index = 0;
 		for (String s: args) {
     			try {
-        			argsToInt.append(Integer.parseInt(args[0]));
+        			argsToInt[index] = Integer.parseInt(s);
     			} catch (NumberFormatException e) {
-        			System.err.println("Argument" + args[0] + " must be an integer.");
+        			System.err.println("Argument" + s + " must be an integer.");
         			System.exit(-1); // we can also comment this line out, to just skip nonvalid inputs
     			}
+    			index++;
 		}
+		return argsToInt;
 	}	
 	
 	
